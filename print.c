cd7f53372d7c8f7a2713c8b32ff0a0d048dfc75f
OrahC <99449327+OrahC@users.noreply.github.com>
2022-04-22 22:02:55 +0100

    Update print.c

--- print.c
+++ print.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
@@ -9,44 +8,40 @@
  * specifiers contained into fmt
  * Return: length of the formatted output string
  */
-
 int _printf(const char *format, ...)
-
 {
-int (*pfunc)(va_list, flags_t *);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-const char *p;
+	register int count = 0;
 
-va_list arguments;
-flags_t flags = {0, 0, 0};
-
-register int count = 0;
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (p = format; *p; p++)
-{
-if (*p == '%')
-{
-p++;
-if (*p == '%')
-{
-count += _putchar('%');
-continue;
-}
-while (get_flag(*p, &flags))
-p++;
-pfunc = get_print(*p);
-count += (pfunc)
-? pfunc(arguments, &flags)
-: _printf("%%%c", *p);
-}
-else
-count += _putchar(*p);
-}
-_putchar(-1);
-va_end(arguments);
-return (count);
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
